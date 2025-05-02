61f068375ed58b1c92d0b1b4065811526ee374c9
ZHAOTranparentAI <zhaotai.liu2409@gmail.com>
2025-05-02 12:06:30 +0200

    entity-level evaluation

--- run_scoring.sh
+++ run_scoring.sh
@@ -3,7 +3,7 @@
 mode="${1:-test}";
 
 python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/prediction
-
+# python src/scoring_entity.py --reference_dir ./data/test_labeled --prediction_dir ./results/deepseek-chat/test_unlabeled --score_dir ./results/scores_entity
 if [ "$mode" = "-debug" ];
 then
     python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/missing_files
