265292fdb7de52c85bdad532049e3d8bf17f2c51
Zongxiong Chen <czxczf@gmail.com>
2025-10-13 18:35:45 +0000

    update

---
+++ run_ft.sh
@@ -0,0 +1,22 @@
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label DATASET
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label CONFERENCE
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label EVALMETRIC
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label LICENSE
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label ONTOLOGY
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label PROGLANG
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label PROJECT
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label PUBLICATION
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label SOFTWARE
+python src/scibert-new.py --mode char --epoch 10 --model roberta-base --target_label WORKSHOP
+
+
+python src/scibert-new.py --mode char --epoch 10 --target_label DATASET
+python src/scibert-new.py --mode char --epoch 10 --target_label CONFERENCE
+python src/scibert-new.py --mode char --epoch 10 --target_label EVALMETRIC
+python src/scibert-new.py --mode char --epoch 10 --target_label LICENSE
+python src/scibert-new.py --mode char --epoch 10 --target_label ONTOLOGY
+python src/scibert-new.py --mode char --epoch 10 --target_label PROGLANG
+python src/scibert-new.py --mode char --epoch 10 --target_label PROJECT
+python src/scibert-new.py --mode char --epoch 10 --target_label PUBLICATION
+python src/scibert-new.py --mode char --epoch 10 --target_label SOFTWARE
+python src/scibert-new.py --mode char --epoch 10 --target_label WORKSHOP
